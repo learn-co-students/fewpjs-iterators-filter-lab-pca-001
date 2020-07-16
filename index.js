4ab68bda41edd8ab77ca87e7dfa856404e844a69
Andrew Kloecker <lt.kvothe@gmail.com>
2020-07-16 16:16:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Code your solution here
+const findMatching = (arr, cond) => arr.filter( a => a.toLowerCase() === cond.toLowerCase());
+const fuzzyMatch = (arr, cond) => arr.filter( a => a.slice(0,cond.length) === cond);
+const matchName = (arr, cond) => arr.filter( a => a.name === cond )
