36c2389cfed06abd2f178f27ffb9bd59caba8155
Jo Fischer <josephinefischer9@gmail.com>
2020-07-17 08:46:03 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  return  drivers.filter(i => {
+    return i.toLowerCase() === string.toLowerCase()
+  })
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter((elem) => elem.indexOf(string) === 0)
+  //The indexOf() method returns the position of the first occurrence of a specified value in a string.
+  // in this case we are saying where the first letter(index 0) of element and string match
+}
+
+function matchName(drivers, string) {
+  return drivers.filter((element) => element.name === string)
+}
