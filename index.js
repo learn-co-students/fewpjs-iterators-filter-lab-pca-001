a900e5e1db582d1619ac27f98b360b0fa8a1a020
Madison McCardle <madison.mccardle@powerhrg.com>
2020-07-17 09:53:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, name){
+  return drivers.filter(drivername => drivername.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.indexOf(letters) === 0)
+}
+
+function matchName(drivers, matchname) {
+  return drivers.filter(driver => driver.name === matchname)
+}
