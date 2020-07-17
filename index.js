07da5081871555ddcaeef8cb5e8b92d56348114c
Idan <idan.zonshein@powerhrg.com>
2020-07-17 11:19:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const { resetHistory } = require("sinon")
+
 // Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(i=> {
+    return i.toLowerCase() === string.toLowerCase()
+  })
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter((i) => i.charAt(0) === string.charAt(0))
+}
+
+function matchName(drivers, string) {
+  return drivers.filter((i) => i.name === string)
+}
